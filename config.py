d524a561f0cea789bb38082b91818c4d2b04f2f7
Mukarram Ali <mukarram.ali@in-Mukkaramali.local>
2018-10-23 12:06:22 +0530

    Stable release

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
 from string import digits
+from IPython import embed
 
 def to_s(list, delim = "\n"):
 	return delim.join(map(str,list))
 
 def sample_text():
 	text = "Journalist Eddie Brock is trying to take down Carlton Drake, the notorious and brilliant founder of the Life Foundation. While investigating one of Drake's experiments, Eddie's body merges with the alien Venom -- leaving him with superhuman strength and power. Twisted, dark and fueled by rage, Venom tries to control the new and dangerous abilities that Eddie finds so intoxicating."
-	return text.translate(None, digits)
+	return text.translate(digits).decode("utf-8")
+
+def breakpoint():
+	embed()
+
--- keywords.py
+++ keywords.py
@@ -1,8 +1,7 @@
 from rake_nltk import Rake
 from nltk.corpus import stopwords 
 from nltk.tokenize import word_tokenize
-
-from config import to_s, sample_text
+from config import to_s, sample_text, breakpoint
 
 def remove_stopwords(text):
 	print("=======Removing stopwords from new article========")
@@ -13,11 +12,9 @@ def remove_stopwords(text):
 
 def ranked_keyword(text):
 	r = Rake()
-	r.extract_keywords_from_text(text)
+	r.extract_keywords_from_text(text.decode("utf-8"))
 	ranked_keyword = r.get_ranked_phrases_with_scores()
-
 	return len(ranked_keyword) < 50 and ranked_keyword or ranked_keyword[0:50]
 
 # keywords = ranked_keyword(sample_text())
-
 # print(to_s(keywords))
